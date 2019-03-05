c14331a74d3a410ed04aca438bf8f5c3673748ef
Ruby Barry <rubypbarry@gmail.com>
2019-03-05 17:43:04 -0500

    project

--- src/List.js
+++ src/List.js
@@ -6,14 +6,14 @@ const List = ({ categories, destroyCat, destroyProd, createProd }) => {
             {
                 categories.map((category) => {
                     return (
-                        <li key={category.id} className="list-group-item">
-                            {category.name} {" "}
+                        <li key={category.id} className="list-group-item" >
+                            {category.name}
                             <button onClick={() => createProd(category.id)} className="btn btn-primary">+</button>
                             <button onClick={() => destroyCat(category.id)} className="btn btn-danger">x</button>
                             <ul className="list-group">
                                 {category.products.map(product => {
                                     return (
-                                        <li key={product.id} className="list-group-item">
+                                        <li key={product.id} className="list-group-item" >
                                             {product.name} {" "}
                                             <button onClick={() => destroyProd(product.id, category.id)} className="btn btn-danger">x</button>
                                         </li>
